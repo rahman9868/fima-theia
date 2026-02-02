8b1b5ac3a99b0f3953f1aaf4db742e6b2f4a8aae
chairul <rahman9868@gmail.com>
2026-02-02 03:44:18 +0000

    fixing

--- lib/main.dart
+++ lib/main.dart
@@ -36,8 +36,11 @@ final GoRouter _router = GoRouter(
     ],
     );
 
+import 'package:intl/date_symbol_data_local.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id_ID', null);
   await Get.putAsync(() => TokenProvider().init());
   await HiveService.init();
   Get.put(AuthController());
